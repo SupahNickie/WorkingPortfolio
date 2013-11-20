03231e7f403c3d0f098e0fce767a9b08349458d8
SupahNickie <nicholascase@live.com>
2013-11-20 15:08:10 -0800

    Changed local database to run off Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,6 @@ ActiveRecord::Schema.define(version: 20131110004344) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
