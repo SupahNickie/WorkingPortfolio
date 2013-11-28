fd99f102ee7aa52d81d1dff9ca9081d83dff5f27
SupahNickie <nicholascase@live.com>
2013-11-27 16:57:00 -0800

    Changed project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 require 'mime/types'
 
 class Project < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :full => "600x450>", :thumb => "300x225>" }
+  has_attached_file :photo, :styles => { :full => "500x375>", :thumb => "300x225>" }
   has_attached_file :mp3
 
 end
