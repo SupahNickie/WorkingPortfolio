c37f553e34caf747fc0ac4159fca568e0ec7dc0c
SupahNickie <nicholascase@live.com>
2013-12-03 17:45:09 -0800

    Making photos bigger

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :photoable, polymorphic: true
-  has_attached_file :photo, :styles => { :full => "500x375>", :thumb => "300x225>" }
+  has_attached_file :photo, :styles => { :full => "700x525>", :thumb => "300x225>" }
 
 end
